3bdba586ba2730c1feaea06d356e204076a2105a
wzulfikar <me@wzulfikar.com>
2020-09-14 05:18:29 +0800

    chore(app): add missing title

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,6 +3,7 @@ import "rodal/lib/rodal.css";
 import "../styles/globals.css";
 import "../styles/cards.css";
 
+import Head from 'next/head'
 import { DefaultSeo } from "next-seo";
 
 import seoConfig from "../src/config/seo";
@@ -10,6 +11,9 @@ import seoConfig from "../src/config/seo";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>{seoConfig.openGraph.title} | React Spring x Tailwind x NextJS</title>
+      </Head>
       <DefaultSeo {...seoConfig} />
       <Component {...pageProps} className="antialiased" />
     </>

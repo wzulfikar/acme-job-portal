3d1bc3aafefa36b259322364879ab128095c9c53
wzulfikar <me@wzulfikar.com>
2020-09-14 05:14:16 +0800

    chore(app): add seo

---
+++ src/config/seo.js
@@ -0,0 +1,19 @@
+export default {
+  openGraph: {
+    title: "Acme Job Portal",
+    description: "Just another experiment. React Spring x Tailwind x NextJS.",
+    url: "https://acme-job-portal.wzulfikar.vercel.app/",
+    images: [
+      {
+        url:
+          "https://acme-job-portal.wzulfikar.vercel.app/assets/images/og-default.jpeg",
+        width: 860,
+        height: 506,
+        alt: `Acme Job Portal`,
+      },
+    ],
+    type: "website",
+    locale: "en_IE",
+    site_name: "acme-job-portal.wzulfikar.vercel.app",
+  },
+};

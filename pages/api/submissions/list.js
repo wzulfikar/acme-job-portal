5acca1c6f36ec66d8b473d397e8a173c258461ba
wzulfikar <me@wzulfikar.com>
2020-09-14 05:09:51 +0800

    chore(api): add dummy api to handle fetch submission list

---
+++ pages/api/submissions/list.js
@@ -0,0 +1,62 @@
+const submissions = [
+  {
+    id: "v1599987288",
+    file_url:
+      "https://res.cloudinary.com/dros7g5rz/image/upload/v1599987288/play/resume-pdf/Grace-ResumeViking-16_cdg0sm.pdf",
+    thumbnail_url:
+      "https://res.cloudinary.com/dros7g5rz/image/upload/v1599987288/play/resume-pdf/Grace-ResumeViking-16_cdg0sm.jpg",
+  },
+  {
+    id: "v1599987287",
+    file_url:
+      "https://res.cloudinary.com/dros7g5rz/image/upload/v1599987287/play/resume-pdf/Grace-ResumeViking-15_v0puiu.pdf",
+    thumbnail_url:
+      "https://res.cloudinary.com/dros7g5rz/image/upload/v1599987287/play/resume-pdf/Grace-ResumeViking-15_v0puiu.jpg",
+  },
+  {
+    id: "v1599987288",
+    file_url:
+      "https://res.cloudinary.com/dros7g5rz/image/upload/v1599987288/play/resume-pdf/Mirna-Davis-Assistant-Manager-Resume-8_wwxcy8.pdf",
+    thumbnail_url:
+      "https://res.cloudinary.com/dros7g5rz/image/upload/v1599987288/play/resume-pdf/Mirna-Davis-Assistant-Manager-Resume-8_wwxcy8.jpg",
+  },
+  {
+    id: "v1599987288",
+    file_url:
+      "https://res.cloudinary.com/dros7g5rz/image/upload/v1599987288/play/resume-pdf/Grace-ResumeViking-10_l3jj0s.pdf",
+    thumbnail_url:
+      "https://res.cloudinary.com/dros7g5rz/image/upload/v1599987288/play/resume-pdf/Grace-ResumeViking-10_l3jj0s.jpg",
+  },
+  {
+    id: "v1599987287",
+    file_url:
+      "https://res.cloudinary.com/dros7g5rz/image/upload/v1599987287/play/resume-pdf/Damien-Smith-Resume-Call-Center-Representative-8_lidyyu.pdf",
+    thumbnail_url:
+      "https://res.cloudinary.com/dros7g5rz/image/upload/v1599987287/play/resume-pdf/Damien-Smith-Resume-Call-Center-Representative-8_lidyyu.jpg",
+  },
+  {
+    id: "v1599987287",
+    file_url:
+      "https://res.cloudinary.com/dros7g5rz/image/upload/v1599987287/play/resume-pdf/George_Dann_-_Resume_-_Office_Clerk-6_go39ks.pdf",
+    thumbnail_url:
+      "https://res.cloudinary.com/dros7g5rz/image/upload/v1599987287/play/resume-pdf/George_Dann_-_Resume_-_Office_Clerk-6_go39ks.jpg",
+  },
+  {
+    id: "v1599987287",
+    file_url:
+      "https://res.cloudinary.com/dros7g5rz/image/upload/v1599987287/play/resume-pdf/Karen_Philips_-_Resume_-_Copywriter-3_ulkfp6.pdf",
+    thumbnail_url:
+      "https://res.cloudinary.com/dros7g5rz/image/upload/v1599987287/play/resume-pdf/Karen_Philips_-_Resume_-_Copywriter-3_ulkfp6.jpg",
+  },
+  {
+    id: "v1599987287",
+    file_url:
+      "https://res.cloudinary.com/dros7g5rz/image/upload/v1599987287/play/resume-pdf/Dwight-Kavanagh-Resume-IT-QA-Analyst-11_z6mvsh.pdf",
+    thumbnail_url:
+      "https://res.cloudinary.com/dros7g5rz/image/upload/v1599987287/play/resume-pdf/Dwight-Kavanagh-Resume-IT-QA-Analyst-11_z6mvsh.jpg",
+  },
+];
+
+export default async (req, res) => {
+  res.send(submissions);
+};
